5ed1d2d9a365f7adc9a4f39124bf6e368336c80c
Unknown <paul.paris@epitech.eu>
2018-06-05 14:34:23 +0200

    feat(Parsing): Created SocketRecv() that reads the server fd and parse

--- src_client_ia/ftp_socket.py
+++ src_client_ia/ftp_socket.py
@@ -3,6 +3,8 @@
 
 import socket
 import sys
+import time
+import errno
 
 
 class FTPSocket:
@@ -15,43 +17,214 @@ class FTPSocket:
         self.socket.connect((self.host, self.port))
 
     def TeamName(self, name):
-        self.socket.send((name + "\n").encode())
+        try:
+            self.socket.send((name + "\n").encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Forward(self):
-        self.socket.send("Forward\n".encode())
+        try:
+            self.socket.send("Forward\n".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Right(self):
-        self.socket.send("Right\n".encode())
+        try:
+            self.socket.send("Right\n".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Left(self):
-        self.socket.send("Left\n".encode())
+        try:
+            self.socket.send("Left\n".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Look(self):
-        self.socket.send("Look\n".encode())
+        try:
+            self.socket.send("Look\n".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Inventory(self):
-        self.socket.send("Inventory\n".encode())
+        try:
+            self.socket.send("Inventory\n".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Broadcast(self, sentence):
-        self.socket.send(("Broadcast " + sentence + "\n").encode())
+        try:
+            self.socket.send(("Broadcast " + sentence + "\n").encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Connect_nbr(self):
-        self.socket.send("Connect_nbr\n".encode())
+        try:
+            self.socket.send("Connect_nbr\n".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Fork(self):
-        self.socket.send("Fork\n".encode())
+        try:
+            self.socket.send("Fork\n".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Eject(self):
-        self.socket.send("Eject\n".encode())
+        try:
+            self.socket.send("Eject\n".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Take(self, item):
-        self.socket.send(("Take " + item + "\n").encode())
+        try:
+            self.socket.send(("Take " + item + "\n").encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Set(self, item):
-        self.socket.send(("Set " + item + "\n").encode())
+        try:
+            self.socket.send(("Set " + item + "\n").encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
 
     def Incantation(self):
-        self.socket.send("Incantation".encode())
+        try:
+            self.socket.send("Incantation".encode())
+        except socket.error:
+            if (self.SocketRecv()[-1] == "dead"):
+                print("YOU'RE DEAD !  ->  ", end="")
+            print("Socket Error")
+            exit(84)
+        except IOError:
+            if IOError.errno == errno.EPIPE:
+                print("Broken Pipe")
+            else:
+                print("Other error")
+
+    def SocketRecv(self):
+        buffer = ""
+        data = ""
+        ret_list = []
+        keep = -1
+        while len(buffer) != keep:
+            try:
+                data = ftp.socket.recv(1024)
+            except socket.error:
+                print("Socket Error")
+                exit(84)
+            except IOError:
+                if IOError.errno == errno.EPIPE:
+                    print("Broken Pipe")
+                else:
+                    print("Other error")
+            if data:
+                buffer += repr(data)[2:-1]
+                keep = len(buffer)
+        ret_list = buffer.split('\\n')
+        i = 0
+        while i < len(ret_list):
+            if (len(ret_list[i]) <= 1):
+                ret_list.pop(i)
+                i -= 1
+            i += 1
+        return ret_list
 
     def __exit__(self):
         self.socket.close()
@@ -67,12 +240,9 @@ ftp.Look()
 ftp.Forward()
 ftp.Look()
 ftp.Inventory()
-buffer = ""
-data = ""
-while True:
-    data = ftp.socket.recv(1024)
-    if data:
-        buffer += repr(data)[2:-1]
-        print(buffer)
-    else:
-        break
+time.sleep(1)
+print(ftp.SocketRecv())
+ftp.Inventory()
+time.sleep(1)
+ftp.Forward()
+time.sleep(0.1)
