6910b311750f97728c28fb38b65df5f398fea90d
VIVIES Antoine <antoine.vivies@epitech.eu>
2018-06-06 11:44:26 +0200

    feat(command): send command python to C feat(connexion) connexion established between Python and C feat(thread) Add python threads for listen server fd all the time

--- src_client_gfx/README.txt
+++
@@ -1,4 +0,0 @@
-To compile the library launch:
-    gcc -fpic --shared $(python3-config --includes) module.c -o sockets.abi3.so
-and then python3:
-    ./hello.py ip:port
--- src_client_gfx/hello.py
+++ src_client_gfx/hello.py
@@ -2,18 +2,56 @@
 
 import sockets
 from sys import argv
+import time
+import signal
+from threading import Thread, RLock
+
+CMD_MSZ = ("msz")
+CMD_BCT = ("bct %d %d")
+CMD_MCT = ("mct")
+CMD_TNA = ("tna")
+CMD_PPO = ("ppo #%d")
+CMD_PLV = ("plv #%d")
+CMD_PIN = ("pin #%d")
+CMD_SGT = ("sgt")
+CMD_SST = ("sst %f")
+lock = RLock()
+
+
+class read_on_server(Thread):
+    def __init__(self, fd):
+        Thread.__init__(self)
+        self.fd = fd
+
+    def run(self):
+        allCommand = list()
+        while (True):
+            with lock:
+                allCommand = sockets.get_fd_activity(
+                    self.fd).split('\n')
+                allCommand = list(filter(None, allCommand))
+                print("allCommand = ", allCommand)
+                time.sleep(0.1)
 
 
 def init_socket(port, ip):
-    if (sockets.create_socket(ip, port) == -1):
+    fd = sockets.create_socket(ip, port)
+    if (fd == -1):
         print("Bad socket init")
         exit(84)
-    return (0)
+    return (fd)
+
 
 def get_command(fd):
-    command = list()
-    while (1):
-        command.append(sockets.get_fd_activity(fd))
+    sockets.send_command(CMD_MSZ, fd)
+    while (True):
+        sockets.send_command(CMD_MCT, fd)
+        time.sleep(0.1)
+
+
+def init_game(fd):
+    sockets.send_command("GFX_CLIENT", fd)
+
 
 def main():
     nb_args = len(argv) - 1
@@ -28,8 +66,16 @@ def main():
         print(e)
         exit(84)
     fd = init_socket(port, ip)
+    thread_1 = read_on_server(fd)
+    thread_1.setDaemon(True)
+    thread_1.start()
+    init_game(fd)
     get_command(fd)
     return (0)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt as k:
+        print("\nProgram interrupt with CTRL+C")
+        exit(0)
