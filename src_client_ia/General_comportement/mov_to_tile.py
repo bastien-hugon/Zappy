520951e7f0b48e15504829d45413e45602fca14c
Unknown <valentin.soyer@epitech.eu>
2018-06-11 23:03:30 +0200

    fix(moving): repare algorithm to move to wanted tile

--- src_client_ia/General_comportement/mov_to_tile.py
+++ src_client_ia/General_comportement/mov_to_tile.py
@@ -9,7 +9,7 @@ def go_to_tile(nb_forward, nb_side, socket):
         resp = []
         while (len(resp) == 0):
             resp = socket.ReadSocket()
-        check_dead(resp)
+            check_dead(resp)
     if (nb_side < 0):
         socket.Left()
         resp = []
@@ -21,22 +21,22 @@ def go_to_tile(nb_forward, nb_side, socket):
         resp = []
         while (len(resp) == 0):
             resp = socket.ReadSocket()
-        check_dead(resp)
+            check_dead(resp)
     nb_side = abs(nb_side)
     for i in range(nb_side):
         socket.Forward()
         resp = []
         while (len(resp) == 0):
             resp = socket.ReadSocket()
-        check_dead(resp)
+            check_dead(resp)
 
 
 def mov_to_tile(tile, level, socket):
     if (tile == 0):
         return
     nb_forward = 0
-    for x in range(1, (level + 1)):
+    for x in range(level + 1):
         if (tile >= (x * (x + 1) - x) and tile <= (x * (x + 1) + x)):
             nb_forward = x
-    nb_side = tile - (level * (level + 1) - level)
+    nb_side = tile - (nb_forward * (nb_forward + 1))
     go_to_tile(nb_forward, nb_side, socket)
