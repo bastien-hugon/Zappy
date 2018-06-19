eee09df6b59fb6eeb6ab8b62a47df19870d5c7f6
Unknown <valentin.soyer@epitech.eu>
2018-06-19 18:13:59 +0200

    feat(Broadcast): parse message received

--- src_client_ia/General_comportement/broadcast.py
+++ src_client_ia/General_comportement/broadcast.py
@@ -22,3 +22,27 @@ def EmptyCacheIgnoreBroadcast(socket):
         check_dead(resp)
     print("resp = " + str(resp))
     return (resp)
+
+
+#
+# @brief: Function that get the message send by broadcast and parse it to see
+# if it can interrest the actual A.I.
+#
+# @param: level [in], actual level of the A.I.
+# @param: message [in], message received
+#
+# @return: Boolean : where the message came from
+# if the message is interresting, -1 otherwise
+#
+def isBroadcastInterresting(level, message):
+    data = message.split(',')
+    broadCast = data[0].split(' ')
+    whereItComeFrom = broadCast[1]
+    message = data[1].split(':')
+    if (broadCast[0] != "message"):
+        return (-1)
+    if (message[0] != " INCANT"):
+        return (-1)
+    if (int(message[1]) != level):
+        return (-1)
+    return (whereItComeFrom)
