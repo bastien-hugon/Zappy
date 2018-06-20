c88995f6b296dd07693585e4358945a8e36601cd
Unknown <paul.paris@epitech.eu>
2018-06-20 15:35:30 +0200

    feat(): added incant and modif

---
+++ src_client_ia/State2/incant.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from General_comportement.ressources import nb_player_here
+from General_comportement.ressources import get_all_item_here
+from General_comportement.broadcast import EmptyCacheIgnoreBroadcast
+
+
+def incant(socket):
+    socket.Look()
+    resp = socket.EmptyCache()
+    resp = resp[:1]
+    if len(resp) > 0 and resp[0] != "ko":
+        resp = resp[0][2:-2].split(',')
+    if (nb_player_here(resp[0]) == 1):
+        get_all_item_here(socket, resp[0])
+        socket.Set('linemate')
+        EmptyCacheIgnoreBroadcast(socket)
+        socket.Incantation()
+        resp = EmptyCacheIgnoreBroadcast(socket)
+        if (resp[0] == "Elevation underway"):
+            socket.EmptyCache()
+            if (resp[0] == "ko"):
+                return (1)
+            else:
+                return (2)
+        else:
+            return (1)
+    else:
+        socket.EmptyCache()
+        return (1)
