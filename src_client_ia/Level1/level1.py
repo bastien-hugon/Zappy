719a3353128ff164224e2993096ad6abfe277669
Unknown <paul.paris@epitech.eu>
2018-06-22 16:14:35 +0200

    feat(update ai): update the ai for level 1 and level 2-7

--- src_client_ia/Level1/level1.py
+++ src_client_ia/Level1/level1.py
@@ -12,26 +12,19 @@ from General_comportement.foodHandling import enough_food
 from General_comportement.foodHandling import enough_food_incant
 
 
-# def enough_stone(linemate):
-#     if (linemate >= 1):
-#         return (True)
-#     else:
-#         return (False)
 def level1(level, socket):
     needed_stones = GetNeededRessources(level)
     while (level == 1):
         inventory = look_inventory(socket)
         food = get_food(inventory)
         left_over = GetLeftOverStone(inventory, needed_stones)
-        # linemate = get_linemate(inventory)
-        print ("food i got: " + str(food))
+        print ("BASE:food i got: " + str(food))
         if (not enough_food(level, food)):
             print('MODE: survive')
             survive_mode(level, food, socket)
         elif (not enough_food_incant(level, food)):
             print('MODE: search food incant')
             search_food_incant_mode(level, food, socket)
-        # elif (not enough_stone(linemate)):
         elif len(left_over) != 0:
             print('MODE: search stone')
             search_stone_mode(level, food, left_over, socket)
