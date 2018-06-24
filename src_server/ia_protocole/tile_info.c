aa8601b3394dc745b173082b9baf4802cfa441ce
Bastien Hugon <bastien.hugon@epitech.eu>
2018-06-24 14:47:01 +0200

    Fix(Map Info): Fixing the Get Tile function in tile_info.c

--- src_server/ia_protocole/tile_info.c
+++ src_server/ia_protocole/tile_info.c
@@ -41,6 +41,10 @@ tile_t get_tile(server_t *srv, int x, int y)
 	if (y < 0)
 		x = (srv->game.height - (-1 * (0 - y)));
 	y %= srv->game.height;
+	if (x >= srv->game.width)
+		x = 0;
+	if (y >= srv->game.height)
+		y = 0;
 	return (srv->game.map[y][x]);
 }
 
